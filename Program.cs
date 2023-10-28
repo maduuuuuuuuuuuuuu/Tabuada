2c6de5ecc591472c5cf8e0720db7097f76a1ac26
maduuuuuuuuuuuuuu <maduamaral2006@gmail.com>
2023-10-28 18:13:01 -0300

    exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int numero, produto;
+int auxiliar = 0;
+    Console.Write("Taboada do número: ");
+    numero = Convert.ToInt32(Console.ReadLine()!);
+
+while(auxiliar <= 10)
+{
+    produto = numero * auxiliar;
+    Console.WriteLine($"{numero} x {auxiliar} = {produto}");
+    auxiliar += 1;
+}
+
